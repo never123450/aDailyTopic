01b9a92c0029bbc244292a4d0ea5d492a10be483
xwy <845619585@qq.com>
2019-04-24 10:07:06 +0800

    segmentTree and trie

--- src/main/java/leecode/Solution347/Solution1.java
+++ src/main/java/leecode/Solution347/Solution1.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
-public class Solution {
+public class Solution1 {
 
     private class Freq implements Comparable<Freq>{
 
