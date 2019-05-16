def275061991666afd13913ff91a1e5667f253a0
darkXu <845619585@qq.com>
2019-05-16 22:26:30 +0800

    array LRU and linkedList LRU

---
+++ src/main/java/LRU/LRUBaseLinkedList.java
@@ -0,0 +1,179 @@
+package one.linkList;
+
+import java.util.Scanner;
+
+/**
+ * @description: 基于单链表的LRU算法
+ * @author: xwy
+ * @create: 3:10 PM 2019/5/16
+ **/
+
+public class LRUBaseLinkedList<T> {
+
+    /**
+     * 默认链表容量
+     */
+    private final static Integer DEFAULT_CAPACITY = 10;
+
+    /**
+     * 头结点
+     */
+    private SNode<T> headNode;
+
+    /**
+     * 聊表长度
+     */
+    private Integer length;
+
+    /**
+     * 链表容量
+     */
+    private Integer capacity;
+
+    public LRUBaseLinkedList() {
+        this.headNode = new SNode<>();
+        this.length = DEFAULT_CAPACITY;
+        this.capacity = 0;
+    }
+
+    public LRUBaseLinkedList(Integer capacity) {
+        this.headNode = new SNode<>();
+        this.length = capacity;
+        this.capacity = 0;
+    }
+
+    public void add(T data) {
+        //查找这个元素是否有前一个节点
+        SNode preNode = findPreNode(data);
+
+        //链表中存在，删除原数据，再插入到链表的头部
+        if (preNode != null) {
+            deleteElement(preNode);
+            insertElementAtBegin(data);
+        } else {
+            if (length >= this.capacity) {
+                //删除尾节点
+                deleteElementAtEnd();
+            }
+            insertElementAtBegin(data);
+        }
+
+    }
+
+    /**
+     * 删除preNode元素的下一个节点
+     *
+     * @param preNode
+     */
+    private void deleteElement(SNode preNode) {
+        preNode.setNext(preNode.getNext().getNext());
+        length--;
+    }
+
+    /**
+     * 删除最后一个元素
+     */
+    private void deleteElementAtEnd() {
+        SNode head = headNode;
+
+        //链表为空直接返回
+        if (head.getNext() == null) {
+            return;
+        }
+
+        while (head.getNext() != null) {
+            head = head.getNext();
+        }
+
+        head.setNext(null);
+        length--;
+
+    }
+
+    private void pringAll() {
+        SNode node = headNode.getNext();
+        System.out.print("head->");
+        while (node != null) {
+            System.out.print(node.getElement() + "->");
+            node = node.getNext();
+        }
+        System.out.print("tail");
+        System.out.println();
+    }
+
+    /**
+     * 链表头部插入元素
+     *
+     * @param data
+     */
+    private void insertElementAtBegin(T data) {
+        headNode.setNext(new SNode(data, headNode.getNext()));
+
+        length++;
+    }
+
+    /**
+     * 获取元素的前一个节点
+     *
+     * @param data
+     * @return
+     */
+    private SNode findPreNode(T data) {
+        SNode node = headNode;
+        while (node.getNext() != null) {
+            if (data.equals(node.getNext().getElement())) {
+                return node;
+            }
+            node = node.getNext();
+        }
+        return null;
+    }
+
+
+    public class SNode<T> {
+        private T element;
+        private SNode next;
+
+        public SNode(T element) {
+            this.element = element;
+        }
+
+        public SNode(T element, SNode next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public SNode() {
+            this.next = null;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public void setElement(T element) {
+            this.element = element;
+        }
+
+        public SNode getNext() {
+            return next;
+        }
+
+        public void setNext(SNode next) {
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUBaseLinkedList linkedList = new LRUBaseLinkedList();
+        Scanner sc = new Scanner(System.in);
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.pringAll();
+
+        linkedList.add(1);
+        linkedList.pringAll();
+    }
+}
---
+++ src/main/java/LRU/LRUBasedArray.java
@@ -0,0 +1,188 @@
+package LRU;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 基于数组实现的LRU缓存
+ * <p>
+ * 1.空间复杂度为O(n)
+ * 2.时间复杂度为O(n)
+ * 3.不支持null的缓存
+ * @author: xwy
+ * @create: 4:27 PM 2019/5/16
+ **/
+
+public class LRUBasedArray<T> {
+
+    private static final int DEFAULT_CAOACITY = (1 << 3);
+
+    private int capacity;
+
+    private int count;
+
+    private T[] value;
+
+    private Map<T, Integer> holder;
+    private boolean full;
+
+    LRUBasedArray() {
+        this(DEFAULT_CAOACITY);
+    }
+
+    public LRUBasedArray(int capacity) {
+        this.capacity = capacity;
+        count = 0;
+        value = (T[]) new Object[capacity];
+        holder = new HashMap<T, Integer>(capacity);
+    }
+
+    /**
+     * 模拟访问某个值
+     *
+     * @param object
+     */
+    public void offer(T object) {
+        if (object == null) {
+            throw new IllegalArgumentException("不能存入null！");
+        }
+        //查询缓存池中是否有这个值
+        Integer index = holder.get(object);
+        //如果没有
+        if (index == null) {
+            //判断是否是满的，满的就移除在缓存
+            if (isFull()) {
+                removeAndCache(object);
+            } else {
+                //不满就直接缓存
+                cache(object, count);
+            }
+        } else {
+            //如果缓存中有，就直接update
+            update(index);
+        }
+    }
+
+    /**
+     * 缓存数据到头部，但要先右移
+     *
+     * @param object
+     * @param count  数组右移的边界
+     */
+    private void cache(T object, int count) {
+        rightShift(count);
+        value[0] = object;
+        holder.put(object, 0);
+        count++;
+
+    }
+
+    /**
+     * 缓存满的情况，踢出后，再缓存到数组头部
+     *
+     * @param object
+     */
+    private void removeAndCache(T object) {
+        T key = value[--count];
+        holder.remove(key);
+        cache(object, count);
+
+    }
+
+    /**
+     * end左边的数据统一右移一位
+     *
+     * @param end
+     */
+    private void rightShift(int end) {
+        for (int i = end - 1; i >= 0; i--) {
+            value[i + 1] = value[i];
+            holder.put(value[i], i + 1);
+        }
+    }
+
+    /**
+     * 缓存中有指定的值就更新
+     *
+     * @param index
+     */
+    private void update(Integer index) {
+        T target = value[index];
+        rightShift(index);
+        value[0] = target;
+        holder.put(target, 0);
+    }
+
+    /**
+     * 判断缓存是否是满的
+     *
+     * @return
+     */
+    public boolean isFull() {
+        return count == capacity;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(value[i]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    static class TestLRUBasedArray {
+
+        public static void main(String[] args) {
+            testDefaultConstructor();
+            testSpecifiedConstructor(4);
+//            testWithException();
+        }
+
+        private static void testWithException() {
+            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
+            lru.offer(null);
+        }
+
+        public static void testDefaultConstructor() {
+            System.out.println("======无参测试========");
+            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
+            lru.offer(1);
+            lru.offer(2);
+            lru.offer(3);
+            lru.offer(4);
+            lru.offer(5);
+            System.out.println(lru);
+            lru.offer(6);
+            lru.offer(7);
+            lru.offer(8);
+            lru.offer(9);
+            System.out.println(lru);
+        }
+
+        public static void testSpecifiedConstructor(int capacity) {
+            System.out.println("======有参测试========");
+            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>(capacity);
+            lru.offer(1);
+            System.out.println(lru);
+            lru.offer(2);
+            System.out.println(lru);
+            lru.offer(3);
+            System.out.println(lru);
+            lru.offer(4);
+            System.out.println(lru);
+            lru.offer(2);
+            System.out.println(lru);
+            lru.offer(4);
+            System.out.println(lru);
+            lru.offer(7);
+            System.out.println(lru);
+            lru.offer(1);
+            System.out.println(lru);
+            lru.offer(2);
+            System.out.println(lru);
+        }
+    }
+
+}
