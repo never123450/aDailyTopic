8b547306073710999d803d2951700782381619ca
darkXu <845619585@qq.com>
2021-01-14 15:46:34 +0800

    2021年01月14日15:46:20

--- src/main/java/one/UnionFind/Main.java
+++ src/main/java/one/UnionFind/Main.java
@@ -4,21 +4,20 @@ import java.util.Random;
 
 public class Main {
 
-    private static double testUF(UF uf, int m){
+    private static double testUF(UF uf, int m) {
 
         int size = uf.getSize();
         Random random = new Random();
 
         long startTime = System.nanoTime();
 
-
-        for(int i = 0 ; i < m ; i ++){
+        for (int i = 0; i < m; i++) {
             int a = random.nextInt(size);
             int b = random.nextInt(size);
             uf.unionElements(a, b);
         }
 
-        for(int i = 0 ; i < m ; i ++){
+        for (int i = 0; i < m; i++) {
             int a = random.nextInt(size);
             int b = random.nextInt(size);
             uf.isConnected(a, b);
@@ -31,27 +30,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // UnionFind1 慢于 UnionFind2
+        //UnionFind1 慢于 UnionFind2
 //        int size = 100000;
 //        int m = 10000;
 
-        // UnionFind2 慢于 UnionFind1, 但UnionFind3最快
+        /**
+         * UnionFind1 : 8.082479074 s
+         * UnionFind2 : 13.50496893 s
+         * UnionFind3 : 0.030206776 s
+         * UnionFind4 : 0.014621048 s
+         * UnionFind5 : 0.016450988 s
+         * UnionFind6 : 0.017077298 s
+         */
         int size = 100000;
         int m = 100000;
 
-//        UnionFind1 uf1 = new UnionFind1(size);
-//        System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
-//
-//        UnionFind2 uf2 = new UnionFind2(size);
-//        System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");
+        // 1 和 2 运行太慢
+        UnionFind1 uf1 = new UnionFind1(size);
+        System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
+
+        UnionFind2 uf2 = new UnionFind2(size);
+        System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");
 
         UnionFind3 uf3 = new UnionFind3(size);
         System.out.println("UnionFind3 : " + testUF(uf3, m) + " s");
 
         UnionFind4 uf4 = new UnionFind4(size);
-        System.out.println("UnionFind4 : " + testUF(uf4,m) + " s");
+        System.out.println("UnionFind4 : " + testUF(uf4, m) + " s");
 
         UnionFind5 uf5 = new UnionFind5(size);
-        System.out.println("UnionFind5 : " + testUF(uf5,m) + " s");
+        System.out.println("UnionFind5 : " + testUF(uf5, m) + " s");
+
+        UnionFind6 uf6 = new UnionFind6(size);
+        System.out.println("UnionFind6 : " + testUF(uf6, m) + " s");
     }
 }
--- src/main/java/one/UnionFind/UF.java
+++ src/main/java/one/UnionFind/UF.java
@@ -1,8 +1,27 @@
 package one.UnionFind;
 
+/**
+ * @description: 并查集接口
+ * @author: xwy
+ * @create: 14:15 2021/1/14
+ **/
+
 public interface UF {
 
     int getSize();
+
+    /**
+     * 是否关联
+     * @param p
+     * @param q
+     * @return
+     */
     boolean isConnected(int p, int q);
+
+    /**
+     * 合并元素p,q
+     * @param p
+     * @param q
+     */
     void unionElements(int p, int q);
 }
